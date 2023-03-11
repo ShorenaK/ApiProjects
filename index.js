1211334268b2c8d1d748594f9506567cc1286de4
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-10 21:42:11 -0500

    fetching api

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ function getIdea() {
 }
 
 
-fetch("https://dog.ceo/api/breeds/image/random")
-.then((respone)=> respone.json())
+// fetch("https://dog.ceo/api/breeds/image/random")
+// .then((respone)=> respone.json())
+// .then((data)=>{
+
+//     console.log(data)
+//     document.getElementById("image-container").innerHTML=
+//     `
+//     <img src="${data.message}" />
+//     `
+// })
+
+fetch("https://apis.scrimba.com/bored/api/activity")
+.then((response)=> response.json())
 .then((data)=>{
-
-    console.log(data)
-    document.getElementById("image-container").innerHTML=
-    `
-    <img src="${data.message}" />
-    `
+   document.getElementById("image-container").innerHTML =
+` <h1> ${data.activity} </h1>`
 })
-
