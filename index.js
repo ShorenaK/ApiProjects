12e619a3857b690b597160f4c49adfc2745c5799
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-10 22:09:12 -0500

    updates

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ function getIdea() {
     fetch("https://www.boredapi.com/api/activity")
         .then(res => res.json())
         .then(data => {
+            console.log(data)
             document.body.classList.add("fun")
             document.getElementById("idea").textContent = data.activity
             document.getElementById("title").textContent = "🦾 HappyBot🦿"
         })
 }
+
+getIdea()
 
 
 // fetch("https://dog.ceo/api/breeds/image/random")
@@ -22,9 +25,9 @@ function getIdea() {
 //     `
 // })
 
-fetch("https://apis.scrimba.com/bored/api/activity")
-.then((response)=> response.json())
-.then((data)=>{
-   document.getElementById("image-container").textContent =
-data.activity
-})
+// fetch("https://apis.scrimba.com/bored/api/activity")
+// .then((response)=> response.json())
+// .then((data)=>{
+//    document.getElementById("image-container").textContent =
+// data.activity
+// })
